bdcd9bde468969df651ebe852b2192397f1ec15f
Govindaroraaa <121595141+Govindaroraaa@users.noreply.github.com>
2024-05-15 23:39:27 +0530

    Created Landing page folder and file
    Created Landing page folder and file

---
+++ services/app/pages/landing/index.vue
@@ -0,0 +1,14 @@
+<template>
+    my landing page
+  </template>
+  
+  <script>
+  export default {
+    name: 'LandingPage'
+  }
+  </script>
+  
+  <style>
+  /* You can write custom CSS here if needed */
+  </style>
+  

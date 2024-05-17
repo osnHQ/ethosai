9ddd530c0cfda630ee7dde04997b3be8221f1a5a
Govindaroraaa <121595141+Govindaroraaa@users.noreply.github.com>
2024-05-18 00:51:00 +0530

    Footer Created

--- services/app/pages/landing/index.vue
+++ services/app/pages/landing/index.vue
@@ -34,7 +34,7 @@
 
 
 <div class="mt-13">
-  <h4 class="w-310 ml-20 font-poppins font-medium text-4.5 leading-10 tracking-wider uppercase text-blue-600">
+  <h4 class="w-310 ml-20 font-poppins font-medium text-4.5 leading-10 tracking-wider uppercase text-[#5C72E7]">
     Join the World’s Largest Decentralized AI Auditing Network
   </h4>
 
@@ -46,6 +46,92 @@
     Our vision is to create the most extensive AI auditing platform on Earth, leveraging community input and blockchain technology to enhance the <br/> transparency and accountability of AI models.
   </p>
 </div>
+
+<footer class="flex justify-between py-16 bg-[#1D2939] text-gray-400 px-20 absolute w-full left-0 top-790 ">
+  
+    <div class="flex flex-col items-start">
+      <div class="footer-logo mb-4">
+        <img src="public/logo.png" alt="OpenQA.AI Logo"> 
+      </div>
+      <p class="text-white">Lorem Ipsum Text Here</p>
+    </div>
+
+    <div class="flex justify-between w-250 max-w-screen-xl">
+      <div>
+        <h2 class=" w-[6.8rem] font-semibold mb-4 text-white">Product</h2>
+        <ul>
+          <li><a href="#" class="block w-[7rem] mb-2 hover:text-gray-300">Overview</a></li>
+          <li><a href="#" class="block w-[6.6rem] mb-2 hover:text-gray-300">Features</a></li>
+          <li><a href="#" class="block w-[10rem] mb-2 hover:text-gray-300">Solutions <span class="bg-white text-[#1D2939] text-sm px-2 py-1 rounded-full">New</span></a></li>
+          <li><a href="#" class="block w-[6.7rem] mb-2 hover:text-gray-300">Tutorials</a></li>
+          <li><a href="#" class="block w-[6rem] mb-2 hover:text-gray-300">Pricing</a></li>
+          <li><a href="#" class="block w-[6.7rem] mb-2 hover:text-gray-300">Releases</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2 class="font-semibold mb-4 text-white">Company</h2>
+        <ul>
+          <li><a href="#" class="block w-[5rem] mb-2 hover:text-gray-300">About us</a></li>
+          <li><a href="#" class="block w-[4.2rem] mb-2 hover:text-gray-300">Careers</a></li>
+          <li><a href="#" class="block w-[3.2rem] mb-2 hover:text-gray-300">Press</a></li>
+          <li><a href="#" class="block w-[3.2rem] mb-2 hover:text-gray-300">News</a></li>
+          <li><a href="#" class="block w-[5rem] mb-2 hover:text-gray-300">Media kit</a></li>
+          <li><a href="#" class="block w-[4.2rem] mb-2 hover:text-gray-300">Contact</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2 class=" w-[5.5rem] font-semibold mb-4 text-white">Resources</h2>
+        <ul>
+          <li><a href="#" class="block w-[3.2rem] mb-2 hover:text-gray-300">Blog</a></li>
+          <li><a href="#" class="block w-[5.9rem] mb-2 hover:text-gray-300">Newsletter</a></li>
+          <li><a href="#" class="block w-[4.2rem] mb-2 hover:text-gray-300">Events</a></li>
+          <li><a href="#" class="block w-[6.4rem] mb-2 hover:text-gray-300">Help centre</a></li>
+          <li><a href="#" class="block w-[5.3rem] mb-2 hover:text-gray-300">Tutorials</a></li>
+          <li><a href="#" class="block w-[5.0rem] mb-2 hover:text-gray-300">Support</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2 class=" w-[5rem] font-semibold mb-4 text-white">Social</h2>
+        <ul>
+          <li><a href="#" class="block w-[5.2rem] mb-2 hover:text-gray-300">Twitter</a></li>
+          <li><a href="#" class="block w-[5.9rem] mb-2 hover:text-gray-300">LinkedIn</a></li>
+          <li><a href="#" class="block w-[6.4rem] mb-2 hover:text-gray-300">Facebook</a></li>
+          <li><a href="#" class="block w-[5.4rem] mb-2 hover:text-gray-300">GitHub</a></li>
+          <li><a href="#" class="block w-[6.4rem] mb-2 hover:text-gray-300">AngelList</a></li>
+          <li><a href="#" class="block w-[6rem] mb-2 hover:text-gray-300">Dribbble</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2 class="w-[3.9rem] font-semibold mb-4 text-white">Legal</h2>
+        <ul>
+          <li><a href="#" class="block w-[3.9rem] mb-2 hover:text-gray-300">Terms</a></li>
+          <li><a href="#" class="block w-[4.5rem] mb-2 hover:text-gray-300">Privacy</a></li>
+          <li><a href="#" class="block w-[4.8rem] mb-2 hover:text-gray-300">Cookies</a></li>
+          <li><a href="#" class="block w-[5rem] mb-2 hover:text-gray-300">Licenses</a></li>
+          <li><a href="#" class="block w-[5rem] mb-2 hover:text-gray-300">Settings</a></li>
+          <li><a href="#" class="block w-[4.8rem] mb-2 hover:text-gray-300">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+
+  <section class="flex items-center py-13 gap-4 absolute w-full h-10 left-0 top-868 bg-gray-900">
+  <div class="flex-grow"></div>
+  <div>
+    <p class="font-inter font-normal text-sm text-gray-400 mr-140">
+      All rights reserved.
+    </p>
+  </div>
+  <div class="flex items-center space-x-4 mr-75">
+    <a href="#"><img class="w-4 h-4" src="public/twitter.png" alt="Social Media Icon 1"></a>
+    <a href="#"><img class="w-4 h-4" src="public/linkedin.png" alt="Social Media Icon 2"></a>
+    <a href="#"><img class="w-4 h-4" src="public/fb.png" alt="Social Media Icon 3"></a>
+    <a href="#"><img class="w-4 h-4" src="public/github.png" alt="Social Media Icon 4"></a>
+    <a href="#"><img class="w-4 h-4" src="public/socialmedia5.png" alt="Social Media Icon 5"></a>
+    <a href="#"><img class="w-4 h-4" src="public/socialmedia6.png" alt="Social Media Icon 6"></a>
+  </div>
+</section>
+
 
 
   </template>

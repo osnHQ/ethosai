6b825f79a6548447f36e49870c6a90dab18f608b
Lulzx <wiserishabh@gmail.com>
2024-05-19 15:44:22 +0530

    improve navbar

--- services/app/pages/landing/Navbar.vue
+++ services/app/pages/landing/Navbar.vue
@@ -1,14 +1,11 @@
 <template>
-    <div>
-        <nav class="relative z-999 ml-15">
-            <a href="" class="mr-10">Home</a>
-            <a href="" class="mr-10">Tokenomics</a>
-            <a href="" class="mr-10">Blog</a>
-            <a href="" class="mr-10">Careers</a>
-            <a href="" class="mr-10">More-></a>
-            <a href="" class="mr-10">Terms and Conditions</a>
-            <a href="" class="mr-10">Privacy Policy</a>
-            <a href="" class="mr-10">Social Media</a>
+    <div class="flex justify-center px-8">
+        <nav class="space-x-8 border-white px-8 py-5 border rounded-full max-w-5xl text-light">
+            <NuxtLink to="/" class="hover:text-gray">Home</NuxtLink>
+            <NuxtLink to="/" class="hover:text-gray">Tokenomics</NuxtLink>
+            <NuxtLink to="/" class="hover:text-gray">Blog</NuxtLink>
+            <NuxtLink to="/" class="hover:text-gray">Careers</NuxtLink>
+            <NuxtLink to="/" class="hover:text-gray">More</NuxtLink>
         </nav>
     </div>
 </template>

e9f35ff251a5506244033bd41e27e049a45ee50f
Lulzx <wiserishabh@gmail.com>
2024-05-19 19:35:41 +0530

    improve get started component

--- services/app/pages/landing/GetStarted.vue
+++ services/app/pages/landing/GetStarted.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="relative bg-gradient-to-r from-purple-500 to-pink-500 mt-12 md:mt-16 lg:mt-20">
-        <div class="top-0 left-0 absolute w-full h-full">
-            <div class="bg-gradient-to-r from-purple-500 to-pink-500 blur-md rounded-2xl w-full h-full"></div>
-            <div class="bg-gray-800 p-4 md:p-6 lg:p-8 rounded-2xl w-full h-full">
-                <div class="font-poppins font-semibold text-2xl text-white md:text-3xl lg:text-4xl leading-snug">
-                    Join our Community of Innovators
-                </div>
-                <div class="font-inter font-light text-center text-lg text-white md:text-xl lg:text-2xl leading-snug">
-                    Discover the power of our platform and revolutionize
-                    language models.
-                </div>
-                <div class="flex justify-center mt-4 md:mt-6 lg:mt-8">
-                    <button
-                        class="bg-white hover:bg-gray-100 px-4 py-2 rounded-full font-poppins font-semibold text-black">
-                        Get Started
-                    </button>
-                    <button
-                        class="bg-white hover:bg-gray-100 ml-4 md:ml-6 lg:ml-8 px-4 py-2 rounded-full font-poppins font-semibold text-black">
-                        Join
-                    </button>
-                </div>
-            </div>
+    <div class="flex justify-center items-center mt-10">
+      <div class="top-0 left-0 absolute blur-md rounded-2xl w-full h-full overlay"></div>
+      <div class="bg-gray-800 px-12 py-8 rounded-2xl w-full md:w-auto lg:min-w-2xl max-w-md text-center">
+        <h1 class="font-poppins font-semibold text-2xl text-white md:text-3xl lg:text-4xl">
+          Join our Community of Innovators
+        </h1>
+        <p class="mt-2 font-inter font-light text-lg text-white md:text-xl lg:text-2xl">
+          Discover the power of our platform and revolutionize language models.
+        </p>
+        <div class="flex justify-center mt-4 md:mt-6 lg:mt-8">
+          <button
+            class="bg-white hover:bg-gray-100 mr-2 px-4 py-2 rounded-full font-poppins font-semibold text-black"
+          >
+            Get Started
+          </button>
+          <button
+            class="bg-white hover:bg-gray-100 px-4 py-2 rounded-full font-poppins font-semibold text-black"
+          >
+            Join
+          </button>
         </div>
+      </div>
     </div>
-</template>
+  </template>

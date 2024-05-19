09f57a4fd013ac129b2cac69175d44c9476d1824
Lulzx <wiserishabh@gmail.com>
2024-05-19 17:00:33 +0530

    improve community component

--- services/app/pages/landing/Community.vue
+++ services/app/pages/landing/Community.vue
@@ -1,73 +1,17 @@
 <template>
-    <div class="mt-13">
-        <h4 class="ml-20 w-310 font-medium font-poppins text-[#5C72E7] text-4.5 uppercase leading-10 tracking-wider">
-            Join the World’s Largest Decentralized AI Auditing Network
-        </h4>
+    <div class="mx-auto mt-24 p-8">
+        <div class="text-sm text-violet-500 uppercase tracking-wider">
+            Join the World's Largest Decentralized AI Auditing Network
+        </div>
 
-        <h1 class="w-350 font-medium text-11 text-center text-white capitalize leading-15">
-            Community insights and blockchain to ensure your AI models
-            <br />
-            are unbiased, accurate, and fully accountable
-        </h1>
+        <div class="mx-auto px-4 pt-2 pb-4 max-w-5xl font-medium text-3xl text-light md:text-4xl leading-relaxed">
+            Community insights and blockchain to ensure your AI models are unbiased, accurate, and fully accountable
+        </div>
 
-        <p class="opacity-80 mt-12 w-350 font-light text-5 text-center text-white leading-5">
+        <p class="mx-auto max-w-3xl font-light text-center text-gray text-md leading-relaxed">
             Our vision is to create the most extensive AI auditing platform
             on Earth, leveraging community input and blockchain technology
-            to enhance the <br />
-            transparency and accountability of AI models.
+            to enhance the transparency and accountability of AI models.
         </p>
     </div>
-
-    <div class="relative custom-gradient mt-25">
-        <div class="left-[50px] w-[100px] h-[280px]">
-            <div class="top-0 w-[1300px] h-[280px]">
-                <div class="bg-gradient blur-30 rounded-[20px] w-full h-full"></div>
-                <div class="bg-[#13111A] rounded-[20px] w-full h-full">
-                    <div
-                        class="top-[calc(50%-34px-8.5px)] left-[20px] w-[791px] h-[68px] font-poppins font-semibold text-[40px] text-white leading-[68px]">
-                        Join our Community of Innovators
-                    </div>
-                    <div
-                        class="top-[180px] left-[15px] opacity-80 w-[726px] h-[27px] font-inter font-light text-[18px] text-center text-white leading-[27px]">
-                        Discover the power of our platform and revolutionize
-                        language models.
-                    </div>
-                    <div class="left-[800px] w-[514px] h-[82.72px]">
-                        <div class="top-[122px] left-0 w-[180px] h-[62px]">
-                            <div class="bg-white rounded-[50px] w-full h-full"></div>
-                            <button
-                                class="top-[15px] left-[15px] w-[151px] h-[38px] font-poppins font-semibold text-[22px] text-black leading-[38px]">
-                                Get Started
-                            </button>
-                        </div>
-                        <div class="top-[122px] left-[244px] w-[180px] h-[62px]">
-                            <div class="border-3 border-white rounded-[50px] w-full h-full"></div>
-                            <button
-                                class="top-[15px] left-[60px] w-[54px] h-[34px] font-poppins font-semibold text-[22px] text-white leading-[38px]">
-                                Join
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
-
-<style>
-.custom-gradient {
-    background: linear-gradient(90deg,
-            rgba(0, 255, 255, 0.3),
-            rgba(255, 0, 255, 0.3));
-}
-
-.bg-gradient {
-    background: linear-gradient(90deg,
-            rgba(138, 121, 255, 0.8) 0%,
-            rgba(206, 77, 164, 0.8) 100%);
-}
-
-.blur-30 {
-    filter: blur(30px);
-}
-</style>
--- services/app/pages/landing/Navbar.vue
+++ services/app/pages/landing/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center px-8">
-        <nav class="space-x-8 border-white px-8 py-5 border rounded-full max-w-5xl text-light">
+        <nav class="space-x-8 border-white px-8 py-4 border rounded-full max-w-3xl text-light">
             <NuxtLink to="/" class="hover:text-gray">Home</NuxtLink>
             <NuxtLink to="/" class="hover:text-gray">Tokenomics</NuxtLink>
             <NuxtLink to="/" class="hover:text-gray">Blog</NuxtLink>

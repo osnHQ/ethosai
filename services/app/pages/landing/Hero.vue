03d7a7439434fb696ec4374a1593679e0252344c
Lulzx <wiserishabh@gmail.com>
2024-05-19 16:54:05 +0530

    improve hero further

--- services/app/pages/landing/Hero.vue
+++ services/app/pages/landing/Hero.vue
@@ -1,37 +1,31 @@
 <template>
-    <div class="mx-auto p-4 pt-20 md:pt-32 container">
-        <div class="mb-4 font-bold text-4xl text-center md:text-5xl">
-            World's first decentralized AI <br />
-            auditing platform backed by <br />
+    <div class="mx-auto container">
+        <div class="mx-auto px-2 md:px-8 pt-24 pb-12 max-w-4xl font-bold text-4xl text-center md:text-5xl">
+            World's first decentralized AI
+            auditing platform backed by
             community
         </div>
 
-        <p class="my-8 text-center text-gray-400 text-lg">
+        <div class="mx-auto px-8 max-w-2xl text-center text-gray-400 text-md md:text-lg">
             Our scalable framework allows for continuous auditing of large
-            language models <br />
-            by evaluating the accuracy and bias of their responses using
-            community-
-            <br />
-            contributed prompts and evaluations.
-        </p>
+            language models by evaluating the accuracy and bias of their responses using
+            community-contributed prompts and evaluations.
+        </div>
 
-        <div class="flex justify-center gap-8">
+        <div class="flex justify-center gap-8 my-8">
             <button
-                class="bg-dark hover:bg-gray-100 mr-4 px-4 py-2 border border-light font-bold text-gray-800 text-light hover:text-dark">
+                class="bg-dark hover:bg-gray-100 mr-4 px-6 py-3 border border-light rounded font-bold text-gray-800 text-light hover:text-dark">
                 Learn More
             </button>
             <button
-                class="bg-dark hover:bg-gray-100 mr-4 px-4 py-2 border border-light font-bold text-gray-800 text-light hover:text-dark">
+                class="bg-dark hover:bg-gray-100 px-6 py-3 border border-light rounded font-bold text-gray-800 text-light hover:text-dark">
                 Sign Up
             </button>
         </div>
 
-        <!--
-        <img
-          src="public/Hero Image.png"
-          alt=""
-          class="block mx-auto mt-25 w-190"
-        />
-      -->
+        <div class="mx-auto my-auto max-w-lg">
+            <img src="public/Hero Image.png" alt="" class="mx-auto" />
+        </div>
+
     </div>
 </template>

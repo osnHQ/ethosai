e1a5c917d222a2ee7af49882b9635f058032a17f
Priyanshi1908 <115202982+Priyanshi1908@users.noreply.github.com>
2024-05-19 13:56:22 +0530

    Priyanshi's components

--- services/app/pages/landing/index.vue
+++ services/app/pages/landing/index.vue
@@ -47,6 +47,261 @@
   </p>
 </div>
 
+<div style="display: flex; justify-content: center; margin-top: 90px; font-family: 'Poppins'">
+    <!-- First card component -->
+    <div
+      style="width: 420px; height: 350px; background-color: #1d1b26; color: white; text-align: center; padding: 20px; margin-right: 70px; border-radius: 50px;">
+
+      <!-- Adding image Accuracy.png -->
+      <img src="public/Accuracy.png" alt="Accuracy"
+        style="width: 80px; height: auto; margin-bottom: 20px; display: block; margin-left: auto; margin-right: auto; top: 20px;">
+
+      <h2 class="text-[35px] font-bold mb-4 mt-[30px]">Accuracy</h2>
+      <p
+        style="font-family: 'Poppins';  font-weight: 100; font-size: 19px; line-height: 33px; color: #FFFFFF80; margin-top:20px">
+        Evaluate and validate language model responses for accuracy and bias with community-contributed prompts.</p>
+    </div>
+
+    <!-- Second card component -->
+    <div
+      style="width: 420px; height: 350px; background-color: #1d1b26; color: white; text-align: center; padding: 20px; margin-left: 70px; border-radius: 50px;">
+      <img src="public/Bias.png" alt="Bias"
+        style="width: 80px; height: auto; margin-bottom: 20px; display: block; margin-left: auto; margin-right: auto; top: 20px;">
+
+      <h2 class="text-[35px] font-bold mb-4 mt-[30px]">Bias-Free</h2>
+      <p
+        style="font-family: 'Poppins';  font-weight: 100; font-size: 19px; line-height: 33px; color: #FFFFFF80; margin-top:20px">
+        Mitigate bias in language models by leveraging community-contributed evaluations and feedback.</p>
+    </div>
+  </div>
+
+  <div style="margin-top: 120px;">
+
+    <h1
+      style="width: 1350px; font-style: normal; font-weight: 600; font-size: 45px; line-height: 76px; text-align: center; text-transform: capitalize; color: #FFFFFF;">
+      Discover How Our Framework Revolutionizes Language Model Auditing
+    </h1>
+
+  </div>
+
+  <div style="display: flex; justify-content: space-between; margin-top: 90px;margin-left:50px;margin-right:50px;">
+
+
+    <!-- First card component -->
+    <div
+      style="width: 355px; height: 430px; background-color: black; color: white; text-align: center; padding: 20px; border-radius: 30px; border: 5px solid transparent; background-clip: padding-box; border-image: linear-gradient(to bottom, #c752ad, #8773f1) 1;">
+
+      <img src="public/Card1.png" alt="Card1"
+        style="width: 80px; height: auto; margin-bottom: 20px; display: block; margin-left: auto; margin-right: auto; transform: translateY(80%);">
+
+      <h2 class="text-[25px] mb-4 mt-[100px]">Submit your prompts</h2>
+
+      <p
+        style="font-family: 'Poppins'; font-weight: 100; font-size: 17px; line-height: 30px; color: #FFFFFF80; margin-top: 30px; width: 100%; margin: 0 auto;">
+        Upload your AI prompts and the expected outputs in a simple configuration file.
+      </p>
+    </div>
+
+    <!-- Second card component -->
+    <div
+      style="width: 355px; height: 430px; background-color: black; color: white; text-align: center; padding: 20px; border-radius: 30px; border: 5px solid transparent; background-clip: padding-box; border-image: linear-gradient(to bottom, #c752ad, #8773f1) 1;">
+
+      <img src="public/Card2.png" alt="Card2"
+        style="width: 80px; height: auto; margin-bottom: 20px; display: block; margin-left: auto; margin-right: auto; transform: translateY(80%);">
+
+      <h2 class="text-[25px] mb-4 mt-[100px]">Community Reviews</h2>
+
+      <p
+        style="font-family: 'Poppins'; font-weight: 100; font-size: 17px; line-height: 30px; color: #FFFFFF80; margin-top: 30px; width: 100%; margin: 0 auto;">
+        Our global community reviews and refines these prompts, ensuring a comprehensive testing approach. </p>
+    </div>
+
+    <!-- Third card component -->
+
+    <div
+      style="width: 355px; height: 430px; background-color: black; color: white; text-align: center; padding: 20px; border-radius: 30px; border: 5px solid transparent; background-clip: padding-box; border-image: linear-gradient(to bottom, #c752ad, #8773f1) 1;">
+
+      <img src="public/Card3.png" alt="Card3"
+        style="width: 80px; height: auto; margin-bottom: 20px; display: block; margin-left: auto; margin-right: auto; transform: translateY(80%);">
+
+      <h2 class="text-[25px] mb-4 mt-[100px]">Earn Tokens</h2>
+
+      <p
+        style="font-family: 'Poppins'; font-weight: 100; font-size: 17px; line-height: 30px; color: #FFFFFF80; margin-top: 30px; width: 100%; margin: 0 auto;">
+        Receive tokens as your contributions help others or as you engage with the platform, enhancing reputations and
+        rewards </p>
+    </div>
+  </div>
+
+
+  <div class="flex w-4.65/5 h-[508px]  mx-auto mt-60px">
+    <div class="flex-1 p-5 overflow-y-auto">
+      <!-- Add your text content here -->
+      <h1
+        style="font-style: normal; font-weight: 600; font-size: 45px; line-height: 1.5; text-transform: capitalize; color: #FFFFFF; margin-right: 150px; margin-bottom: 0;">
+        OpenQA’s Scoring Engine
+      </h1>
+      <p
+        style="font-family: 'Poppins'; font-weight: 100; font-size: 18px; color: #FFFFFF80; margin-top: 10px; margin-bottom: 0; text-align: justify; margin-left:28px;">
+        Our state-of-the-art Scoring Engine precisely measures the deviation between the AI-generated output and the
+        ideal expected output. This distance-based method quantifies the accuracy of the AI response, ensuring a
+        rigorous evaluation standard.
+      </p>
+
+      <div class="flex items-start mt-8 ml-8 h-25">
+        <div class="relative w-auto h-auto mb-30">
+          <img src="public/Precision.png" alt="Precision" class="object-cover w-25 h-auto">
+          <img src="public/clock.png" alt="Clock image"
+            class="absolute w-15 h-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        </div>
+        <div class="ml-4">
+          <h2 class="text-[22px]  text-justify">Precision</h2>
+          <p class="text-[16px] font-poppins font-light text-justify text-[#FFFFFF80]">Accurately pinpoints
+            discrepancies to ensure each response meets the highest quality standards.</p>
+        </div>
+      </div>
+
+      <div class="flex items-start mt-8 ml-8  h-25">
+        <div class="relative w-auto h-auto mb-30">
+          <img src="public/Precision.png" alt="Precision" class="object-cover w-22 h-auto">
+          <img src="public/Cross.png" alt="Cross image"
+            class="absolute w-15 h-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        </div>
+        <div class="ml-4">
+          <h2 class="text-[22px]  text-justify">Reliability</h2>
+          <p class="text-[16px] font-poppins font-light text-justify text-[#FFFFFF80]">
+            Consistent and dependable scoring that users can trust for evaluating AI outputs.
+          </p>
+        </div>
+
+
+      </div>
+
+
+    </div>
+    <div class="flex justify-end items-center p-5">
+      <!-- Add your image here -->
+      <img src="public/ScoringEngine.png" alt="Description of the image" class="max-w-full max-h-full object-contain">
+    </div>
+  </div>
+
+  <!-- 2ND COMPONENT -->
+
+  <div class="flex w-4.65/5 h-[508px]  mx-auto mt-60px">
+
+    <div class="flex justify-end items-center p-5">
+      <!-- Add your image here -->
+      <img src="public/ScoringEngine2.png" alt="ScoringEngine2" class="max-w-full max-h-full object-contain ml-8">
+    </div>
+
+    <div class="flex-1 p-5 overflow-y-auto ml-40">
+
+
+      <div class="flex items-start mt-8 ml-8 h-30 ">
+        <div class="relative w-auto h-auto mb-30">
+          <img src="public/Precision.png" alt="Precision" class="object-cover w-60 h-auto mt-6">
+          <img src="public/clock.png" alt="Clock image"
+            class="absolute w-12 h-auto top-1.3/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        </div>
+        <div class="ml-4">
+          <h2 class="text-[22px]  text-justify">Bias Free Testing</h2>
+          <p class="text-[16px] font-poppins font-light text-justify text-[#FFFFFF80]">
+            Our scalable framework allows for continuous auditing of large language models by evaluating
+            the accuracy and bias of their responses using community-contributed prompts and evaluations.
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start mt-8 ml-8  h-30">
+        <div class="relative w-auto h-auto mb-30">
+          <img src="public/Precision.png" alt="Precision" class="object-cover w-60 h-auto mt-6">
+          <img src="public/Cross.png" alt="Cross image"
+            class="absolute w-12 h-auto top-1.3/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        </div>
+        <div class="ml-4">
+          <h2 class="text-[22px]  text-justify">Accuracy Assurance</h2>
+          <p class="text-[16px] font-poppins font-light text-justify text-[#FFFFFF80]">
+            Utilize sophisticated algorithms to measure the 'distance' between the AI's response and the expected
+            output.
+            This rigorous validation process guarantees that the AI models perform accurately and reliably. </p>
+        </div>
+      </div>
+
+      <div class="flex items-start mt-8 ml-8 h-30">
+        <div class="relative w-auto h-auto mb-30">
+          <img src="public/Precision.png" alt="Precision" class="object-cover w-60 h-auto mt-6">
+          <img src="public/Cross.png" alt="Cross image"
+            class="absolute w-12 h-auto top-1.3/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        </div>
+        <div class="ml-4">
+          <h2 class="text-[22px]  text-justify">Tokenization</h2>
+          <p class="text-[16px] font-poppins font-light text-justify text-[#FFFFFF80]">
+            Engage with our token system designed to reward contributions and facilitate robust community interaction.
+            Tokens not only incentivize participation but also help sustain and grow the platform's ecosystem.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="mt-20">
+  
+
+  <h1 class="w-350 font-medium text-11 leading-15 text-center capitalize text-white">
+    Stay Informed With our Blogs
+  </h1>
+
+  <p class="w-350 mt-12 font-light text-5 leading-5 text-center text-white opacity-80">
+    Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia et sint laboriosam sed ipsa sint ut voluptatum labore et.
+  </p>
+</div>
+
+<div class="blog-wrapper">
+    <div class="blog-card">
+      <div class="card-img">
+        <img src="public/blog1.jpeg" />
+      </div>
+      <div class="card-details">
+        <span><i class="fa fa-calendar"></i>AUG 4</span><span><i class="fa fa-heart"></i>102</span>
+      </div>
+      <div class="card-text">
+        <h1>Blockchain</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si verbum sequimur, primum longius verbum praepositum quam bonum.</p>
+      </div>
+      <div class="read-more">Read More</div>
+    </div>
+    <div class="blog-card">
+      <div class="card-img">
+        <img src="public/blog2.jpeg" />
+      </div>
+      <div class="card-details">
+        <span><i class="fa fa-calendar"></i>JAN 3</span><span><i class="fa fa-heart"></i>119</span>
+      </div>
+      <div class="card-text">
+        <h1>New York</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si verbum sequimur, primum longius verbum praepositum quam bonum.</p>
+      </div>
+      <div class="read-more">Read More</div>
+    </div>
+    <div class="blog-card">
+      <div class="card-img">
+        <img src="public/blog3.jpeg" />
+      </div>
+      <div class="card-details">
+        <span><i class="fa fa-calendar"></i>AUG 19</span><span><i class="fa fa-heart"></i>265</span>
+      </div>
+      <div class="card-text">
+        <h1>San Francisco</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si verbum sequimur, primum longius verbum praepositum quam bonum.</p>
+      </div>
+      <div class="read-more">Read More</div>
+    </div>
+  </div>
+      <!-- PRI'S COMPONENTS END HERE -->
+
+
+
 <div class="relative custom-gradient mt-25">
   <div class="absolute w-[100px] h-[280px] left-[50px] ">
         <div class="absolute w-[1300px] h-[280px]  top-0">
@@ -122,7 +377,7 @@
         </div>
     </div>
 
-<footer class="flex justify-between py-16 bg-[#1D2939] text-gray-400 px-20 absolute w-full left-0 top-790 ">
+<footer class="flex justify-between py-16 bg-[#1D2939] text-gray-400 px-20 absolute w-full left-0 top-1550 ">
   
     <div class="flex flex-col items-start">
       <div class="footer-logo mb-4">
@@ -190,7 +445,7 @@
     </div>
   </footer>
 
-  <section class="flex items-center py-13 gap-4 absolute w-full h-10 left-0 top-868 bg-gray-900">
+  <section class="flex items-center py-13 gap-4 absolute w-full h-10 left-0 top-1628 bg-gray-900">
   <div class="flex-grow"></div>
   <div>
     <p class="font-inter font-normal text-sm text-gray-400 mr-140">
@@ -218,10 +473,9 @@
   </script>
   
   <style>
-  /* You can write custom CSS here if needed */
   body {
   /* Set background image */
-  background-image: url('public/Home.png'); /* Adjust the path to your image */
+  background-image: url('public/Home.jpeg'); /* Adjust the path to your image */
   /* Optional: Adjust background size, position, and repeat */
   background-size: cover; /* Cover the entire viewport */
   background-position: top; /* Center the image */
@@ -233,6 +487,8 @@
   padding: 0;
   /* Set body height to 100vh to cover full viewport height */
   height: 500vh;
+  color: rgb(228, 227, 248);
+
 }
 
 .custom-gradient {
@@ -252,4 +508,122 @@
         }
 
   </style>
-  
+
+
+
+<!--CSS FOR BLOG CARDS-->
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Oswald|Roboto|Slabo+27px');
+@import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+.blog-wrapper {
+  padding: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.blog-card {
+  transition: all 0.3s;
+  max-width: 300px;
+  margin: 35px;
+  background: #1d1b26;
+  border: 1px solid #410072;
+  text-align: center;
+  cursor: pointer;
+}
+
+.blog-card:hover {
+  box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.1);
+}
+
+.blog-card:hover .card-img img {
+  opacity: 0.8;
+}
+
+.card-img {
+  position: relative;
+  text-align: center;
+  background: #410072;
+}
+
+.card-img img {
+  transition: all 0.3s;
+  max-height: 180px;
+  width: 100%;
+  border-bottom: 4px solid #410072;
+}
+
+.card-img::before {
+  content: '';
+  position: absolute;
+  bottom: -8px;
+  left: 50%;
+  margin-left: -10px;
+  width: 0;
+  height: 0;
+  border-top: solid 10px #57009a;
+  border-left: solid 10px transparent;
+  border-right: solid 10px transparent;
+}
+
+.card-img h1 {
+  position: absolute;
+  margin: 0;
+  font-size: 42px;
+  bottom: 15px;
+  width: 100%;
+  color: #fff;
+  font-family: 'Slabo 27px', serif;
+}
+
+.card-details {
+  margin-top: 30px;
+  font-family: 'Roboto', sans-serif;
+  color: #e3dff0;
+}
+
+.card-details span {
+  padding: 0 30px;
+}
+
+.card-details i {
+  margin-right: 5px;
+}
+
+.card-text {
+  padding: 30px 15px;
+  font-family: 'Roboto', sans-serif;
+  line-height: 22px;
+}
+.card-text h1{
+  font-size: 24px; /* Increased font size */
+  padding: 10px 10px; /* Decreased padding */
+  margin: 10px 0; 
+  font-family: 'Roboto', sans-serif;
+  color: #fff; /* White text color for better neon effect */
+  text-shadow: 
+  0 0 2px rgba(255, 0, 255, 0.5), 
+    0 0 5px rgba(255, 0, 255, 0.5), 
+    0 0 10px rgba(255, 0, 255, 0.5);
+}
+
+.read-more {
+  transition: all 0.3s;
+  display: inline-block;
+  width: auto;
+  text-align: center;
+  text-transform: uppercase;
+  background: #410072;
+  color: #fff;
+  padding: 15px;
+  margin-bottom: 30px;
+  font-family: 'Oswald', sans-serif;
+}
+
+.read-more:hover {
+  background: #410072;
+}
+</style>

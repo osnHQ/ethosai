0dff1f35bbed1792261ff1088cd2c42eed4cd564
Lulzx <wiserishabh@gmail.com>
2024-05-19 19:17:01 +0530

    improve features icons

--- services/app/pages/landing/Features.vue
+++ services/app/pages/landing/Features.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-center mt-10 font-poppins">
+    <div class="flex md:flex-row flex-col justify-center mt-10">
         <div class="bg-[#1d1b26] mx-4 mb-6 px-8 py-12 rounded-[50px] w-full max-w-md h-auto text-center text-white">
             <img src="public/Accuracy.png" alt="Accuracy" class="mx-auto mb-5 w-20">
             <h2 class="mt-8 mb-4 font-bold text-2xl">Accuracy</h2>
@@ -65,13 +65,13 @@
         <div class="flex-1 ml-10 p-5 overflow-y-auto">
             <div class="flex items-start mt-8 h-30">
                 <div class="relative mb-6 w-auto h-auto">
-                    <img src="public/Precision.png" alt="Precision" class="mt-6 w-15 h-auto object-cover">
+                    <img src="public/Precision.png" alt="Precision" class="w-25 h-auto object-cover">
                     <img src="public/clock.png" alt="Clock image"
-                        class="top-1/2 left-1/2 absolute w-12 h-auto transform -translate-x-1/2 -translate-y-1/2">
+                        class="top-1/2 left-1/2 absolute p-2 w-15 h-auto transform -translate-x-1/2 -translate-y-1/2">
                 </div>
                 <div class="ml-4">
-                    <h2 class="text-lg">Bias Free Testing</h2>
-                    <p class="font-light text-justify text-sm text-opacity-80">Our scalable framework allows for
+                    <h2 class="mb-2 text-left text-lg">Bias Free Testing</h2>
+                    <p class="font-light text-gray text-justify text-sm text-opacity-80">Our scalable framework allows for
                         continuous auditing of large language models by evaluating the accuracy and bias of their
                         responses using community-contributed prompts and evaluations.</p>
                 </div>
@@ -79,13 +79,13 @@
 
             <div class="flex items-start mt-8 h-30">
                 <div class="relative mb-6 w-auto h-auto">
-                    <img src="public/Precision.png" alt="Precision" class="mt-6 w-15 h-auto object-cover">
+                    <img src="public/Precision.png" alt="Precision" class="w-25 h-auto object-cover">
                     <img src="public/Cross.png" alt="Cross image"
-                        class="top-1/2 left-1/2 absolute w-12 h-auto transform -translate-x-1/2 -translate-y-1/2">
+                        class="top-1/2 left-1/2 absolute p-2 w-15 h-auto transform -translate-x-1/2 -translate-y-1/2">
                 </div>
                 <div class="ml-4">
-                    <h2 class="text-lg">Accuracy Assurance</h2>
-                    <p class="font-light text-justify text-sm text-opacity-80">Utilize sophisticated algorithms to
+                    <h2 class="mb-2 text-left text-lg">Accuracy Assurance</h2>
+                    <p class="font-light text-gray text-justify text-sm text-opacity-80">Utilize sophisticated algorithms to
                         measure the 'distance' between the AI's response and the expected output. This rigorous
                         validation process guarantees that the AI models perform accurately and reliably.</p>
                 </div>
@@ -93,13 +93,13 @@
 
             <div class="flex items-start mt-8 h-30">
                 <div class="relative mb-6 w-auto h-auto">
-                    <img src="public/Precision.png" alt="Precision" class="mt-6 w-15 h-auto object-cover">
+                    <img src="public/Precision.png" alt="Precision" class="w-25 h-auto object-cover">
                     <img src="public/Cross.png" alt="Cross image"
-                        class="top-1/2 left-1/2 absolute w-12 h-auto transform -translate-x-1/2 -translate-y-1/2">
+                        class="top-1/2 left-1/2 absolute p-2 w-15 h-auto transform -translate-x-1/2 -translate-y-1/2">
                 </div>
                 <div class="ml-4">
-                    <h2 class="text-lg">Tokenization</h2>
-                    <p class="font-light text-justify text-sm text-opacity-80">Earn tokens and build your reputation
+                    <h2 class="mb-2 text-left text-lg">Tokenization</h2>
+                    <p class="font-light text-gray text-justify text-sm text-opacity-80">Earn tokens and build your reputation
                         within our community by contributing to the platform through submitting, reviewing, and refining
                         prompts, as well as engaging in other activities.</p>
                 </div>

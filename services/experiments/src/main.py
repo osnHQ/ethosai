4e6673cc2b576afa3ab32b2c6e383de011f45bba
Lulzx <wiserishabh@gmail.com>
2024-05-21 10:49:07 +0530

    use streamit for ui

--- services/experiments/src/main.py
+++ services/experiments/src/main.py
@@ -1,56 +1,59 @@
+import streamlit as st
+import pandas as pd
 import json
 import ollama
-import pandas
 import asyncio
 from numpy import dot
 from numpy.linalg import norm
 from ollama import AsyncClient
 
-from pathlib import Path
+st.title("OpenQA.ai")
 
-file_path = Path("./qa.jsonl")
-
-def load_jsonl():
-    with file_path.open("r") as f:
-        contents = f.read()
-        jsonl_data = []
-        lines = contents.splitlines()
-
-        for line in lines:
-            jsonl_data.append(json.loads(line))
-
-        return jsonl_data
-
+uploaded_file = st.file_uploader("Upload a JSONL file", type=["jsonl"])
 
 async def chat(prompt):
-  message = {'role': 'user', 'content': prompt}
-  response = await AsyncClient().chat(model='qwen:0.5b', messages=[message])
-  return response["message"]["content"]
-
+    message = {'role': 'user', 'content': prompt}
+    response = await AsyncClient().chat(model='qwen:0.5b', messages=[message])
+    return response["message"]["content"]
 
 def generate_embedding(prompt):
     response = ollama.embeddings(model='qwen:0.5b', prompt=prompt)
     return response["embedding"]
 
-
 def cosine_similarity(a, b):
     return dot(a, b) / (norm(a) * norm(b))
-
 
 def sentence_similarity(a, b):
     x = generate_embedding(a)
     y = generate_embedding(b)
     return cosine_similarity(x, y)
 
+def process_jsonl(file):
+    jsonl_data = []
+    for line in file:
+        jsonl_data.append(json.loads(line.decode("utf-8")))
+    return jsonl_data
 
-def main():
-    jsonl_data =  load_jsonl()
-    responses = []
-    for pair in jsonl_data:
-        response = asyncio.run(chat(pair["question"]))
-        print(response)
-        responses.append(response)
-        print(pair["answer"])
-        print(sentence_similarity(response, pair["answer"]))
+if uploaded_file is not None:
+    jsonl_data = process_jsonl(uploaded_file)
+    df = pd.json_normalize(jsonl_data)
+    st.write(df)
 
-main()
+    async def main():
+        results = []
+        table_container = st.empty()
+        
+        for pair in jsonl_data:
+            response = await chat(pair["question"])
+            similarity = sentence_similarity(response, pair["answer"])
+            results.append({
+                "Question": pair["question"],
+                "Model Response": response,
+                "Expected Answer": pair["answer"],
+                "Similarity": f"{similarity:.4f}"
+            })
+            
+            results_df = pd.DataFrame(results)
+            table_container.table(results_df)
+
+    asyncio.run(main())

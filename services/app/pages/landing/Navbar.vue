6baec46b44514b46a5d93ede5e33af55198bcd44
Priyanshi1908 <115202982+Priyanshi1908@users.noreply.github.com>
2024-05-23 23:18:35 +0530

    added logo in navbar

--- services/app/pages/landing/Navbar.vue
+++ services/app/pages/landing/Navbar.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="flex justify-center px-8 py-10">
-        <nav class="space-x-8 border-white px-8 py-4 border rounded-full max-w-3xl text-light">
+    <div class="flex justify-between items-center px-8 py-10">
+        <img src="public/logo.png" alt="OpenQA.AI Logo" class="ml-[-20px] mr-3 md:ml-0 md:mr-0" />
+        <nav class="space-x-8 border-white px-8 py-4  border rounded-full max-w-3xl mx-auto text-light">
             <NuxtLink to="/" class="hover:text-gray">Home</NuxtLink>
             <NuxtLink to="https://openqas-organization.gitbook.io/openqa.ai-whitepaper-1" class="hover:text-gray">Whitepaper</NuxtLink>
             <!-- <NuxtLink to="/" class="hover:text-gray">Blog</NuxtLink> -->
             <NuxtLink to="https://www.linkedin.com/company/openqa-ai/jobs/" class="hover:text-gray">Careers</NuxtLink>
-            <NuxtLink to="mailto: openqaai@gmail.com" class="hover:text-gray">Contact</NuxtLink>
+            <NuxtLink to="mailto:openqaai@gmail.com" class="hover:text-gray">Contact</NuxtLink>
         </nav>
     </div>
 </template>
+

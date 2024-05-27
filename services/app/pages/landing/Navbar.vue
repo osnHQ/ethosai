39a5637432aaa1d2c880273d54bdcce6c233ae6f
Priyanshi1908 <115202982+Priyanshi1908@users.noreply.github.com>
2024-05-27 22:58:17 +0530

    Added link to navbar logo

--- services/app/pages/landing/Navbar.vue
+++ services/app/pages/landing/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="flex items-center px-8 py-10">
-        <img src="public/logo.png" alt="OpenQA.AI Logo" class="ml-[-20px] w-12 h-12 mr-3 md:ml-0 md:mr-0 md:w-20 md:h-20" />
+        <a href="/">
+    <img src="public/logo.png" alt="OpenQA.AI Logo" class="ml-[-20px] w-12 h-12 mr-3 md:ml-0 md:mr-0 md:w-20 md:h-20" />
+</a>
         <!-- <nav class="space-x-8 border-white px-8 py-4 border rounded-full max-w-3xl mx-auto text-light"> -->
             <nav class="space-x-3 text-[13px] border-white px-5 py-3 border rounded-full max-w-3xl mx-auto text-light md:space-x-8 md:text-[20px]  md:py-4">
 

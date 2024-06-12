af4fd2efcba7ac7ec3fa0420cf5308d67995a301
faridul haque <faridulhaquemrushed@gmail.com>
2024-06-13 01:16:52 +0600

    hero section's text and image have been placed in row

--- services/app/pages/landing/Hero.vue
+++ services/app/pages/landing/Hero.vue
@@ -1,29 +1,42 @@
 <template>
-    <div class="mx-auto container">
-        <div class="mx-auto px-2 md:px-8 pt-12 pb-12 max-w-4xl font-bold text-4xl text-center md:text-5xl">
-            Creating safe AI future with our Decentralised LLM Auditing Infrastructure
+    <div class="lg:h-[80vh] container mx-auto lg:flex flex-row items-center justify-between">
+        <div class="w-1/2">
+            <div class=" pt-12 pb-12 max-w-4xl font-bold text-4xl md:text-5xl">
+                Creating safe AI future with our Decentralised LLM Auditing Infrastructure
+            </div>
+
+            <div class=" text-gray-400 text-md md:text-lg">
+                ethosAI tackles the key challenge in AI: building the infrastructure for safe and rapid innovation. We
+                leverage
+                a unique approach -a <b>decentralized community of users</b> who identify issues in AI systems. <b>Their
+                    insights</b> are then reviewed and compiled into a massive database fed into <b>ethosAI LLM
+                    decentralised
+                    auditing infrastructure</b>, continuously improving AI fairness and reliability for everyone.
+            </div>
+
+            <div class="flex justify-start">
+                <a href="https://ethos-ai.gitbook.io/ethosai-whitepaper" class="flex justify-center my-8 lg:mr-5">
+                    <button
+                        class="bg-transparent hover:bg-gray-100 mr-4 px-6 py-3 border border-light rounded font-bold text-gray-800 text-light hover:text-dark">
+                        Whitepaper
+                    </button>
+                </a>
+                <a href="https://tally.so/r/nGrMK2" class="flex justify-center my-8">
+                    <button
+                        class="bg-dark hover:bg-gray-100 px-6 py-3 border border-light rounded font-bold text-gray-800 text-light hover:text-dark">
+                        Join Waitlist
+                    </button>
+                </a>
+            </div>
+
+
+
         </div>
 
-        <div class="mx-auto px-8 max-w-2xl text-center text-gray-400 text-md md:text-lg">
-            ethosAI tackles the key challenge in AI: building the infrastructure for safe and rapid innovation.  We leverage a unique approach -a <b>decentralized community of users</b> who identify issues in AI systems. <b>Their insights</b> are then reviewed and compiled into a massive database fed into <b>ethosAI LLM decentralised auditing infrastructure</b>, continuously improving AI fairness and reliability for everyone.
-        </div>
-
-    <div class="flex justify-center gap-9">
-     <a href="https://ethos-ai.gitbook.io/ethosai-whitepaper" class="flex justify-center gap-8 my-8">
-        <button class="bg-transparent hover:bg-gray-100 mr-4 px-6 py-3 border border-light rounded font-bold text-gray-800 text-light hover:text-dark">
-            Whitepaper
-        </button>
-     </a>
-     <a href="https://tally.so/r/nGrMK2" class="flex justify-center gap-8 my-8">
-        <button class="bg-dark hover:bg-gray-100 px-6 py-3 border border-light rounded font-bold text-gray-800 text-light hover:text-dark">
-            Join Waitlist
-        </button>
-     </a>
-    </div>
 
 
-        <div class="mx-auto my-auto max-w-lg">
-            <img src="public/Hero Image.png" alt="" class="mx-auto" />
+        <div class="max-w-lg w-1/2 flex items-center justify-center">
+            <img src="public/Hero Image.png" alt="" />
         </div>
 
     </div>

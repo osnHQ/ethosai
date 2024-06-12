ed1cd5ec62baef4ba28eef0bd870a4069d2f88cb
faridul haque <faridulhaquemrushed@gmail.com>
2024-06-13 00:49:20 +0600

    navbar's logo has been placed in the left

--- services/app/pages/landing/Navbar.vue
+++ services/app/pages/landing/Navbar.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="flex justify-center px-1 py-10">
+  <div class="container h-20 mx-auto relative">
+    <!-- logo fixed with position -->
+    <div class="flex items-center justify-center mt-5 h-1 absolute left-0">
+      <a class="" href="https://openqa.ai/">
+        <img src="public/logo.png" class="w-35" alt="ethosAI Logo">
+      </a>
+    </div>
 
-    <nav class="space-x-8 border-white px-2 py-4 md-px-8 md-py-4 border rounded-full max-w-3xl text-light">
-      <NuxtLink to="/" class="hover:text-gray">Home</NuxtLink>
-      <NuxtLink to="https://ethos-ai.gitbook.io/ethosai-whitepaper" class="hover:text-gray">Whitepaper
-      </NuxtLink>
-      <!-- <NuxtLink to="/" class="hover:text-gray">Blog</NuxtLink> -->
-      <NuxtLink to="https://www.linkedin.com/company/ethosaione/jobs/" class="hover:text-gray">Careers</NuxtLink>
-      <NuxtLink to="mailto: contactethosai@gmail.com" class="hover:text-gray">Contact</NuxtLink>
-    </nav>
-  </div>
-  <div class="flex items-center justify-center mt-5 h-1">
-    <a href="https://openqa.ai/">
-      <img src="public/logo.png" class="w-35" alt="ethosAI Logo">
-    </a>
+    <div class="flex justify-center items-center mt-10">
+
+      <nav class="space-x-8 border-white px-2 md-px-8 md-py-4 border rounded-full max-w-3xl text-light">
+        <NuxtLink to="/" class="hover:text-gray">Home</NuxtLink>
+        <NuxtLink to="https://ethos-ai.gitbook.io/ethosai-whitepaper" class="hover:text-gray">Whitepaper
+        </NuxtLink>
+        <!-- <NuxtLink to="/" class="hover:text-gray">Blog</NuxtLink> -->
+        <NuxtLink to="https://www.linkedin.com/company/ethosaione/jobs/" class="hover:text-gray">Careers</NuxtLink>
+        <NuxtLink to="mailto: contactethosai@gmail.com" class="hover:text-gray">Contact</NuxtLink>
+      </nav>
+    </div>
   </div>
 </template>

c9cddd18688549b58310d672bc5067c8f07f0bd6
Parag Arora <parag.arora@gmail.com>
2024-06-15 22:05:55 +0530

    update hero

--- services/app/pages/landing/Hero.vue
+++ services/app/pages/landing/Hero.vue
@@ -5,15 +5,15 @@
         <div class="max-md-w-full sm:w-full lg:w-1/2">
 
             <div class="max-md-text-center sm:text-center lg:text-left pt-12 pb-12 max-w-4xl font-bold max-md-text-4xl md:text-3xl lg:text-4xl">
-                Creating safe AI future with our Decentralized LLM Auditing Infrastructure
+                World's first decentralised AI Audit Network
             </div>
 
             <div class="max-md-text-center sm:text-center lg:text-left text-gray-400 text-md md:text-lg">
                 ethosAI tackles the key challenge in AI: building the infrastructure for safe and rapid innovation. We
                 leverage
-                a unique approach -a <b>decentralized community of users</b> who identify issues in AI systems. <b>Their
-                    insights</b> are then reviewed and compiled into a massive database fed into <b>ethosAI LLM
-                    decentralised
+                a unique approach -a <b>decentralized auditing network</b> to identify issues in AI systems. <b>Network
+                    insights</b> are then zkRolled up for proof of work, reviewed and compiled into a massive database fed into <b>ethosAI 
+                    decentralised AI 
                     auditing infrastructure</b>, continuously improving AI fairness and reliability for everyone.
             </div>
 

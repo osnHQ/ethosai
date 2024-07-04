99273e09c81d913f4f71e4fe1e491a294e8878ef
Govindaroraaa <aroraa5810@gmail.com>
2024-07-04 13:47:33 +0530

    Partners added

--- services/app/pages/index.vue
+++ services/app/pages/index.vue
@@ -4,11 +4,12 @@
             <ClientOnly>
                 <div>
                     <Navbar />
-                    <Background/>
+                    <Background />
                     <Hero />
                     <Community />
                     <Features />
                     <GetStarted />
+                    <Partnerships />
                     <FAQ />
                     <Footer />
                 </div>
@@ -23,14 +24,15 @@
 </template>
 
 <script setup>
-import Navbar from "./landing/Navbar";
-import Hero from "./landing/Hero";
-import Community from "./landing/Community";
-import Features from "./landing/Features";
-import GetStarted from "./landing/GetStarted";
-import FAQ from "./landing/FAQ";
-import Footer from "./landing/Footer";
-import Background from "./landing/Background.vue";
+import Navbar from './landing/Navbar'
+import Hero from './landing/Hero'
+import Community from './landing/Community'
+import Features from './landing/Features'
+import GetStarted from './landing/GetStarted'
+import FAQ from './landing/FAQ'
+import Footer from './landing/Footer'
+import Background from './landing/Background.vue'
+import Partnerships from './landing/Partnerships.vue'
 
 const color = useColorMode()
 color.value === 'dark'
---
+++ services/app/pages/landing/Partnerships.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="flex flex-col items-center p-4 text-center">
+    <div class="mx-auto max-w-screen-lg font-semibold text-3xl text-white md:text-4xl leading-tight mt-12">
+        Partnerships and Integrations
+        </div>
+    <div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6">
+      <div class="flex items-center justify-center p-4">
+        <img src="public/gcloud.png" alt="Google Cloud" class="w-full h-auto" />
+      </div>
+      <div class="flex items-center justify-center p-4">
+        <img src="public/corover.png" alt="Corover AI" class="w-full h-auto" />
+      </div>
+      <div class="flex items-center justify-center p-4">
+        <img src="public/chatgpt.png" alt="ChatGPT" class="w-full h-auto" />
+      </div>
+      <div class="flex items-center justify-center p-4">
+        <img src="public/aws.png" alt="AWS Partner" class="w-full h-auto" />
+      </div>
+      <div class="flex items-center justify-center p-4">
+        <img src="public/microsoft.png" alt="Microsoft" class="w-full h-auto" />
+      </div>
+      <div class="flex items-center justify-center p-4">
+        <img src="public/meta.png" alt="Meta" class="w-full h-auto" />
+      </div>
+    </div>
+  </section>
+</template>

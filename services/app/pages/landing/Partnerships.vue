b25908eb3d991fa1142a5595bd18571e293bf241
Govindaroraaa <121595141+Govindaroraaa@users.noreply.github.com>
2024-07-17 15:52:32 +0530

    Govind's PR (#34)
    * BharatGPT logo added to partnerships
    
    * create config page created
    
    * CSS updated

--- services/app/pages/landing/Partnerships.vue
+++ services/app/pages/landing/Partnerships.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto max-w-screen-lg font-semibold text-3xl text-white md:text-4xl leading-tight mt-12">
         Partnerships and Integrations
         </div>
-    <div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6">
+    <div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-7 mx-5">
       <div class="flex items-center justify-center p-4">
         <img src="public/gcloud.png" alt="Google Cloud" class="w-full h-auto" />
       </div>
@@ -18,6 +18,9 @@
       </div>
       <div class="flex items-center justify-center p-4">
         <img src="public/microsoft.png" alt="Microsoft" class="w-full h-auto" />
+      </div>
+      <div class="flex items-center justify-center p-4">
+        <img src="public/bharatgpt.png" alt="Meta" class="w-full h-auto" />
       </div>
       <div class="flex items-center justify-center p-4">
         <img src="public/meta.png" alt="Meta" class="w-full h-auto" />
